ac892d303316bb7509bb6e5ffbd7fbf822bc4625
Kristina Stojic <stojic.kris@gmail.com>
2022-07-18 15:55:23 +0200

    feat: login - frontend

--- posta-frontend/src/app/app-routing.module.ts
+++ posta-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
+import { AdminHomeComponent } from './component/admin-home/admin-home.component';
+import { LoginComponent } from './component/login/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent,
+    pathMatch: 'full', 
+  },
+  {
+    path: 'admin-home',
+    component: AdminHomeComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- posta-frontend/src/app/app.module.ts
+++ posta-frontend/src/app/app.module.ts
@@ -5,17 +5,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './component/login/login.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import {AngularMaterialModule} from './angular-material/angular-material.module'; 
+import { ReactiveFormsModule } from '@angular/forms';
+import { AdminHomeComponent } from './component/admin-home/admin-home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    AdminHomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-
+    NoopAnimationsModule,
+    AngularMaterialModule,
+    ReactiveFormsModule
 
   ],
   providers: [],
