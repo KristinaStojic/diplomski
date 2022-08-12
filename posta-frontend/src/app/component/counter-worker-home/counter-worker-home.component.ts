8fd2a9f7236f9264bb42bed73f19b501901ee8a9
Kristina Stojic <stojic.kris@gmail.com>
2022-08-12 19:07:21 +0200

    feat: chart with number of payments by year

--- posta-frontend/src/app/component/counter-worker-home/counter-worker-home.component.ts
+++ posta-frontend/src/app/component/counter-worker-home/counter-worker-home.component.ts
@@ -1,4 +1,7 @@
+import { PaymentService } from './../../service/payment.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import Chart from 'chart.js/auto';
 
 @Component({
   selector: 'app-counter-worker-home',
@@ -7,9 +10,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CounterWorkerHomeComponent implements OnInit {
 
-  constructor() { }
+  canvas: any;
+  ctx: any;
+  id;
+  myChart: any;
+
+  constructor(private router: Router, private paymentService: PaymentService) { }
 
   ngOnInit(): void {
+    this.paymentService.getNumberofPaymentsYearly().subscribe((data : any) => {
+
+      console.log(data)
+    let keys = Object.keys(data)
+    let values = Object.values(data)
+    this.canvas = document.getElementById('myChartYear');
+    this.ctx = this.canvas.getContext('2d');
+    let myChartYear = new Chart(this.ctx, {
+      type: 'bar',
+      data: {
+          labels: keys,
+          datasets: [{
+              label: '# of Reservations',
+              data: values,
+              backgroundColor: [
+                  'rgba(255, 99, 132, 1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(54, 100, 235, 1)'
+              ],
+              borderWidth: 1,
+
+          }],
+      },
+
+      options: {
+        responsive: false,
+        display:true,
+
+      }
+    });
+
+    })
   }
 
+  allPayments(){
+    this.router.navigate(['/payments']);
+
+  }
 }
