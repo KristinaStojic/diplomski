28eec7e2c25226e36c5e750b70b05467fb2474f9
Kristina Stojic <stojic.kris@gmail.com>
2022-08-28 15:51:08 +0200

    feat: search payoffs

--- posta-backend/Posta/src/main/java/com/example/posta/controller/PayoffController.java
+++ posta-backend/Posta/src/main/java/com/example/posta/controller/PayoffController.java
@@ -1,12 +1,12 @@
 package com.example.posta.controller;
 
-import com.example.posta.dto.PaymentDTO;
-import com.example.posta.dto.PayoffDTO;
+import com.example.posta.dto.*;
 import com.example.posta.service.PaymentService;
 import com.example.posta.service.PayoffService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,6 +23,29 @@ public class PayoffController {
     public @ResponseBody
     ResponseEntity<List<PayoffDTO>> getAllPayoffs(@PathVariable String worker){
         List<PayoffDTO> p = payoffService.getAllPayoffs(worker);
+        if(p != null){
+            return new ResponseEntity<>(p, HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+
+    @RequestMapping(value="/changePaidOffStatus/{id}", method = RequestMethod.PUT)
+    @PreAuthorize("hasAuthority('ROLE_COUNTER_WORKER')")
+    public @ResponseBody ResponseEntity<Boolean> changePaidOffStatus(@PathVariable Long id){
+        Boolean p = payoffService.changePaidOffStatus(id);
+        if(p){
+            return new ResponseEntity<>(true, HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+
+
+    @RequestMapping(value="/search", method = RequestMethod.POST)
+    @PreAuthorize("hasAuthority('ROLE_COUNTER_WORKER')")
+    public ResponseEntity<List<PayoffDTO>> search(@RequestBody SearchPayoffDTO dto){
+        List<PayoffDTO> p = payoffService.search(dto);
         if(p != null){
             return new ResponseEntity<>(p, HttpStatus.OK);
         }
--- posta-backend/Posta/src/main/java/com/example/posta/service/PayoffService.java
+++ posta-backend/Posta/src/main/java/com/example/posta/service/PayoffService.java
@@ -1,6 +1,7 @@
 package com.example.posta.service;
 
 import com.example.posta.dto.PayoffDTO;
+import com.example.posta.dto.SearchPayoffDTO;
 import com.example.posta.model.Payoff;
 import com.example.posta.model.PostOffice;
 import com.example.posta.model.Worker;
@@ -10,6 +11,7 @@ import com.example.posta.repository.WorkerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -47,4 +49,42 @@ public class PayoffService {
 
         return ret;
     }
+
+
+    public Boolean changePaidOffStatus(Long id){
+        Payoff p = payoffRepository.findById(id).orElseGet(null);
+        if(p == null){
+            return false;
+        }
+
+        p.setPaidOff(true);
+        p.setDate(LocalDateTime.now());
+        payoffRepository.save(p);
+
+        return true;
+    }
+
+
+    public List<PayoffDTO> search(SearchPayoffDTO dto){
+        List<PayoffDTO> ret = new ArrayList<>();
+
+        Worker w = workerRepository.findByEmail(dto.getWorker());
+        if(w == null){
+            return null;
+        }
+
+        for(Payoff p: payoffRepository.findAll()){
+            if(p.getCounterWorker().getPostOffice().getId() == w.getPostOffice().getId()){
+                if(p.getClient().getName().toUpperCase().contains(dto.getCriteria().toUpperCase())
+                        || p.getClient().getSurname().toUpperCase().contains(dto.getCriteria().toUpperCase())
+                            || p.getClient().getAddress().getStreet().toUpperCase().contains(dto.getCriteria().toUpperCase())
+                                || p.getClient().getAddress().getStreetNumber().toUpperCase().contains(dto.getCriteria().toUpperCase())){
+                    PayoffDTO po = new PayoffDTO(p);
+                    ret.add(po);
+                }
+            }
+        }
+        return ret;
+    }
+
 }
